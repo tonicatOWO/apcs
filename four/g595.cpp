3de6d503d61d3a03a4c61ef6680f75bce659e45d
TommcyOWO <tangminghong.tom@gmail.com>
2025-02-24 14:41:42 +0800

    update ~

--- four/g595.cpp
+++ four/g595.cpp
@@ -1,21 +1,23 @@
-#include <iostream>  
-using namespace std;  
-
-int main() {  
-    int n, h[105];  
-
-    cin >> n;  
-    for(int i = 0; i < n; i++) {  
-        cin >> h[i];  
-    }  
-
-    int sum = 0;  
-    for(int i = 0; i < n; i++) {  
-        sum += h[i];  
-    }  
-
-    cout << sum << endl;  
-
-    return 0;  
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (void){
+    int n;
+    cin >> n;
+    int h[n];
+    int total = 0; 
+    int temp;
+     
+    for(int i=0;i<n;i++) cin >> h[i];
+    for(int i=1;i<n-1;i++){
+        if(h[i] == 0){
+            temp = min(h[i-1],h[i+1]);
+            total += temp;
+        }
+    } 
+    if(h[0] == 0) total += h[1]; 
+    if(h[n-1] == 0) total += h[n-2];
+    
+    cout << total << endl;
+    return 0; 
 }
-
