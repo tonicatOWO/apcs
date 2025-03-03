22c5013766b87602419b302c3f4d9de6a23a3325
TommcyOWO <tangminghong.tom@gmail.com>
2025-03-03 14:44:01 +0800

    update > e787 finish

--- four/e787.cpp
+++ four/e787.cpp
@@ -1,20 +1,42 @@
-#include <iostream>  
-using namespace std;  
+#include <iostream>
+using namespace std; 
 
 int main() {  
-  int n,m;
+  int n, m;
   cin >> n >> m;
-  int mp[150][105],t[105][105];
-  for (int i=0;i<n;i++){
-    for (int j=0;j<m;j++) cin>>mp[i][j];
-  }
-  for (int i=0;i<n;i++){
-    for (int j=0;j<m;j++) cin>>t[i][j];
-  }
-  int sum;
-  for (int i=0;i<n;i++){
-    for (int j=0;j<m;j++) {
-      std::cout << mp[i][j];
+  
+  int mp[105][105];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> mp[i][j];
     }
   }
+  
+  int t[105][105];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> t[i][j];
+    }
+  }
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      int sum = 0;
+      
+      for (int c = 0; c < m; c++) {
+        sum += t[i][c];
+      }
+      
+      for (int f = 0; f < n; f++) {
+        sum += t[f][j];
+      }
+      
+      sum -= t[i][j];
+            
+      cout << (sum % 2 == 1) ? !mp[i][j] : mp[i][j]; << " ";
+    }
+    cout << endl;
+  }
+   
+  return 0;
 }
