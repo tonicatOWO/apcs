8729c99a7eeaae7aab46b18913cd56457e2b8098
TommcyOWO <tangminghong.tom@gmail.com>
2025-03-03 15:16:08 +0800

    update > setup o077

---
+++ four/o077.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int H,W,N,r,c,t,x;
+  std::cin >> H >> W >> N;
+  int d[H][W];
+
+  while (N--) {
+    std::cin >> r >> c >> t >> x;
+    for (size_t i = 0; i < length; i++) {
+      
+    }
+  }
+}
