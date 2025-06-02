f0e84a060eb25b558cf26871b0737d159a075ee1
tonicatOWO <tonicatowo@proton.me>
2025-06-02 15:10:54 +0800

    update < add d323/ v1&v2

--- Advanced/one/d323.cpp
+++ Advanced/one/d323.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// v1
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -17,5 +19,28 @@ int main() {
   for (int i = 0; i < n; i++) {
     cout << v[i] << " ";
   }
+  return 0;
+}
+
+// v2
+
+int v2main () {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+
+  vector<int> v(n);
+
+  for (auto &i : v) {
+    cin >> i;
+  }
+
+  sort(v.begin(), v.end());
+
+  for (const auto &i : v) {
+    cout << i << " ";
+  }
+
   return 0;
 }
