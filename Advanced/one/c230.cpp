453f113d66627abe104ba9082f97fec946fa9ec6
tonicatOWO <tonicatowo@proton.me>
2025-06-02 14:52:00 +0800

    update < add d323 & fix c230

--- Advanced/one/c230.cpp
+++ Advanced/one/c230.cpp
@@ -3,53 +3,32 @@ using namespace std;
 
 struct Tree {
   int x, y, h;
-  long long dist_sq;
-  int index;
-
-  bool operator<(const Tree &other) const { return dist_sq < other.dist_sq; }
 };
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cout.tie(0);
 
   int n;
-  cin >> n;
+  while (cin >> n) {
+    vector<Tree> trees(n);
 
-  vector<Tree> trees(n);
-
-  for (int i = 0; i < n; i++) {
-    cin >> trees[i].x >> trees[i].y >> trees[i].h;
-    trees[i].index = i;
-    trees[i].dist_sq =
-        (long long)trees[i].x * trees[i].x + (long long)trees[i].y * trees[i].y;
-  }
-
-  sort(trees.begin(), trees.end());
-
-  int start_pos = -1, end_pos = -1;
-  for (int i = 0; i < n; i++) {
-    if (trees[i].index == 0)
-      start_pos = i;
-    if (trees[i].index == n - 1)
-      end_pos = i;
-  }
-
-  vector<long long> dp(n, -1);
-  dp[start_pos] = 0;
-
-  for (int i = 0; i < n; i++) {
-    if (dp[i] == -1)
-      continue;
-
-    for (int j = i + 1; j < n; j++) {
-      long long fun = max(0, trees[i].h - trees[j].h);
-      dp[j] = max(dp[j], dp[i] + fun);
+    for (int i = 0; i < n; i++) {
+      cin >> trees[i].x >> trees[i].y >> trees[i].h;
     }
+
+    sort(trees.begin(), trees.end(),
+         [](const Tree &a, const Tree &b) { return a.x + a.y < b.x + b.y; });
+
+    int max_height = -1;
+    int max_diff = -1;
+
+    for (int i = 0; i < n; i++) {
+      max_diff = max(max_height - trees[i].h, max_diff);
+      max_height = max(max_height, trees[i].h);
+    }
+
+    cout << max_diff << endl;
   }
-
-  cout << dp[end_pos] << endl;
-
   return 0;
 }
