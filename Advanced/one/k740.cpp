4fa7280100c4efedddc09d0c4bac71286aed5a70
tonicatOWO <tonicatowo@proton.me>
2025-06-09 15:02:25 +0800

    update < add k740v2

--- Advanced/one/k740.cpp
+++ Advanced/one/k740.cpp
@@ -25,3 +25,31 @@ int main() {
   }
   return 0;
 }
+
+// version 2.0
+
+int mainV2() {
+  int n;
+  cin >> n;
+
+  vector<vector<int>> v(n, vector<int>(n));
+  for (int i; i < n; i++) {
+    v[i].resize(i + 1);
+    v[i][0] = 1;
+    v[i][i] = 1;
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j <= i; j++) {
+      v[i][j] = v[i - 1][j - 1] + v[i - 1][j];
+    }
+  }
+
+  for (const auto &row : v) {
+    for (int val : row) {
+      if (val != 0)
+        cout << val << " ";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
